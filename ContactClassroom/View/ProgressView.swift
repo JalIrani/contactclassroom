b605bd28e3acf69690d13f12e272d1591301f3d3
Jal Irani <jal@jals-mbp.lan>
2020-06-26 21:43:07 -0400

    contact card completed

--- ContactClassroom/View/ProgressView.swift
+++ ContactClassroom/View/ProgressView.swift
@@ -12,32 +12,111 @@ struct ProgressView: View {
     @State var progressValue: Float = 0.11
     
     var body: some View {
-        ZStack {
-            Color.yellow
-                .opacity(0.1)
-                .edgesIgnoringSafeArea(.all)
-            
+        VStack {
             VStack {
+                Text("Contact Card")
+                    .font(.system(size: 45, weight: .bold, design: .default))
+                    .foregroundColor(Color.black)
+                Text("Below is your score for the last 24 hours")
+                    .font(.system(size: 10, weight: .semibold, design: .default))
                 ProgressBar(progress: self.$progressValue)
-                    .frame(width: 150.0, height: 150.0)
+                    .frame(width: 175.0, height: 175.0)
                     .padding(40.0)
-                
-//                Button(action: {
-//                    self.incrementProgress()
-//                }) {
-//                    HStack {
-//                        Image(systemName: "plus.rectangle.fill")
-//                        Text("Increment")
-//                    }
-//                    .padding(15.0)
-//                    .overlay(
-//                        RoundedRectangle(cornerRadius: 15.0)
-//                            .stroke(lineWidth: 2.0)
-//                    )
-//                }
-                
-                Spacer()
             }
+            VStack {
+                HStack {
+                    Text("Last 24 hours")
+                    .font(.system(size: 17, weight: .black, design: .default))
+                    .frame(alignment: .leading)
+                    Spacer()
+                }
+                Spacer()
+                    .frame(height: 5)
+                HStack {
+                    Text("7800 York Rd")
+                        .padding(.horizontal)
+                        .font(.system(size: 15, weight: .medium, design: .default))
+                    Spacer()
+                    Text("5")
+                        .padding(.horizontal)
+                        .font(.system(size: 15, weight: .semibold, design: .default))
+                }
+                HStack {
+                    Text("Hawkings Hall")
+                        .padding(.horizontal)
+                        .font(.system(size: 15, weight: .medium, design: .default))
+                    Spacer()
+                    Text("1")
+                        .padding(.horizontal)
+                        .font(.system(size: 15, weight: .semibold, design: .default))
+                }
+                HStack {
+                    Text("Stephens Hall")
+                        .padding(.horizontal)
+                        .font(.system(size: 15, weight: .medium, design: .default))
+                    Spacer()
+                    Text("9")
+                        .padding(.horizontal)
+                        .font(.system(size: 15, weight: .semibold, design: .default))
+                }
+                HStack {
+                    Text("Union Garage")
+                        .padding(.horizontal)
+                        .font(.system(size: 15, weight: .medium, design: .default))
+                    Spacer()
+                    Text("3")
+                        .padding(.horizontal)
+                        .font(.system(size: 15, weight: .semibold, design: .default))
+                }
+                HStack {
+                    Text("Burdick Hall")
+                        .padding(.horizontal)
+                        .font(.system(size: 15, weight: .medium, design: .default))
+                    Spacer()
+                    Text("10")
+                        .padding(.horizontal)
+                        .font(.system(size: 15, weight: .semibold, design: .default))
+                }
+                HStack {
+                    Text("University Union")
+                        .padding(.horizontal)
+                        .font(.system(size: 15, weight: .medium, design: .default))
+                    Spacer()
+                    Text("8")
+                        .padding(.horizontal)
+                        .font(.system(size: 15, weight: .semibold, design: .default))
+                }
+                HStack {
+                    Text("Freedom Square")
+                        .padding(.horizontal)
+                        .font(.system(size: 15, weight: .medium, design: .default))
+                    Spacer()
+                    Text("6")
+                        .padding(.horizontal)
+                        .font(.system(size: 15, weight: .semibold, design: .default))
+                }
+
+                HStack {
+                    Text("Average")
+                        .font(.system(size: 16, weight: .bold, design: .default))
+                        .frame(alignment: .leading)
+                    Spacer()
+                    Text("5.5")
+                    .padding(.horizontal)
+                    .font(.system(size: 15, weight: .bold, design: .default))
+                }
+            }
+            Divider()
+            VStack (alignment: .leading) {
+                Text("Recommendations")
+                .font(.system(size: 17, weight: .black, design: .default))
+                .frame(alignment: .leading)
+                Text("Based on your campus activity, you should avoid walking through Freedom sqaure to get to Stephens Hall. Using the pathway near Liberal Arts building should accomplish this.")
+                    .padding(.horizontal)
+                    .font(.system(size: 15, weight: .medium, design: .default))
+                .frame(alignment: .leading)
+            }
+            Spacer()
         }
     }
     
@@ -54,12 +133,12 @@ struct ProgressBar: View {
             Circle()
                 .stroke(lineWidth: 20.0)
                 .opacity(0.3)
-                .foregroundColor(Color.red)
+                .foregroundColor(Color.green)
             
             Circle()
                 .trim(from: 0.0, to: CGFloat(min(self.progress, 1.0)))
                 .stroke(style: StrokeStyle(lineWidth: 20.0, lineCap: .round, lineJoin: .round))
-                .foregroundColor(Color.red)
+                .foregroundColor(Color.green)
                 .rotationEffect(Angle(degrees: 270.0))
                 .animation(.linear)
 
